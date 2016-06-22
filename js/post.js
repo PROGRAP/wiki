7cd696b108ed087b71af32e73cb9697a8324e401
jeremy-seipelt <jeremy.seipelt@getcore.io>
2016-06-22 12:44:11 +0200

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+window.addEventListener('change', function(){
   toc = document.getElementById('markdown-toc');
   console.log(toc);
   panel = document.getElementById('insert-toc');
